d227ffbf4f3ce8bc2215e7ae75075461ed6c818d
mehonator <mehonator@yandex.ru>
2021-09-28 21:31:50 +0300

    create custom user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Role(models.TextChoices):
+    USER = "user", _("User")
+    ADMIN = "admin", _("Admin")
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_("email address"), blank=False, unique=True)
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.USER,
+    )
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_admin(self):
+        return self.role == Role.ADMIN or self.is_superuser or self.is_staff
