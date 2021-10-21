02b3a8bf6c11aae5de7848ad1e8be00265c8d41d
mehonator <mehonator@yandex.ru>
2021-10-21 21:56:58 +0300

    добавил вьюсеты в урл

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,16 +1,28 @@
 from django.urls import include, path
+from django.urls.conf import re_path
 from rest_framework.routers import DefaultRouter
 
-from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
+from api.views import (
+    IngredientViewSet,
+    RecipeViewSet,
+    SubscriptionCreateDestroy,
+    SubscriptionList,
+    TagViewSet,
+)
 
 app_name = "api"
 
 router = DefaultRouter()
-router.register("ingredients", IngredientViewSet, basename="ingredient")
+router.register("ingredients", IngredientViewSet, basename="ingredients")
 router.register("tags", TagViewSet, basename="tags")
 router.register("recipes", RecipeViewSet, basename="recipes")
 
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("users/subscriptions/", SubscriptionList.as_view()),
+    re_path(
+        r"users/(?P<user_id>\d+)/subscriptions",
+        SubscriptionCreateDestroy.as_view(),
+    ),
 ]
