157901660fcee41264e83d55d8f2107b428c5f2d
mehonator <mehonator@yandex.ru>
2021-11-13 14:55:57 +0300

    user factory

---
+++ backend/users/tests/factories.py
@@ -0,0 +1,26 @@
+import factory
+from users.models import CustomUser
+
+
+class CustomUserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = CustomUser
+
+    username = factory.Sequence(lambda n: f"username_{n}")
+    email = factory.Sequence(lambda n: f"username_{n}@email.com")
+    first_name = factory.Sequence(lambda n: f"Имя_{n}")
+    last_name = factory.Sequence(lambda n: f"Фамилия_{n}")
+
+    @staticmethod
+    def user_to_dict(user: CustomUser, request_user: CustomUser) -> dict:
+        is_subscribed = user.leader_subscriptions.filter(
+            follower=request_user
+        ).exists()
+        return {
+            "email": user.email,
+            "id": user.id,
+            "username": user.username,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "is_subscribed": is_subscribed,
+        }
