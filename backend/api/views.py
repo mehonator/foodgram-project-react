ddaab2cae21661e6b53fb16a1802b367b941bbfc
mehonator <mehonator@yandex.ru>
2021-12-07 13:47:38 +0300

    refactoring

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,6 @@
 import io
 from collections import OrderedDict
 
-import django_filters
 from django.contrib.auth import get_user_model
 from django.http.response import FileResponse
 from fpdf import FPDF
@@ -12,12 +11,13 @@ from rest_framework.generics import (
     ListAPIView,
     get_object_or_404,
 )
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from api.constants import IS_FAVORITED_VALUES, IS_IN_SHOPING_CART_VALUES
+from api.filters import IngredientFilter, RecipeFilter
 from api.models import Ingredient, Recipe, Subscription, Tag
+from api.pagintors import CustomLimitOffsetPagination
 from api.permissions import IsAuthor
 from api.serializers import (
     IngredientSerializer,
@@ -30,10 +30,6 @@ from api.serializers import (
 from api.validations import ValidationResult, validate_query_params
 
 CustomUser = get_user_model()
-
-
-class CustomLimitOffsetPagination(LimitOffsetPagination):
-    default_limit = 100
 
 
 class ListRetrievDestroyViewSet(
@@ -51,31 +47,6 @@ class ListRetrievViewSet(
     viewsets.GenericViewSet,
 ):
     pass
-
-
-class RecipeFilter(django_filters.FilterSet):
-    author = django_filters.NumberFilter()
-    tags = django_filters.AllValuesMultipleFilter(field_name="tags__slug")
-    is_favorited = django_filters.CharFilter(method="get_is_favorited")
-    is_in_shopping_cart = django_filters.CharFilter(
-        method="get_is_in_shopping_cart"
-    )
-
-    class Meta:
-        model = Recipe
-        fields = ("author", "tags", "is_favorited")
-
-    def get_is_favorited(self, queryset, name, value):
-        if IS_FAVORITED_VALUES[value]:
-            return queryset.filter(users_chose_as_favorite=self.request.user)
-        else:
-            return queryset.exclude(users_chose_as_favorite=self.request.user)
-
-    def get_is_in_shopping_cart(self, queryset, name, value):
-        if IS_IN_SHOPING_CART_VALUES[value]:
-            return queryset.filter(users_put_in_cart=self.request.user)
-        else:
-            return queryset.exclude(users_put_in_cart=self.request.user)
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -310,14 +281,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         ] = 'attachment; filename="shopong-list.pdf"'
 
         return response
-
-
-class IngredientFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr="istartswith")
-
-    class Meta:
-        model = Ingredient
-        fields = ["name"]
 
 
 class IngredientViewSet(ListRetrievViewSet):
