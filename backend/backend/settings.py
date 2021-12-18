7be69a2ffe826e06301a3f4d983bbe01bc43369d
mehonator <mehonator@yandex.ru>
2021-12-18 12:48:14 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
 
 
 INSTALLED_APPS = [
