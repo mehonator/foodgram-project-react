24c17c624eb5c1e8b62efd23953528d72d52c0ca
mehonator <mehonator@yandex.ru>
2021-12-19 18:35:53 +0300

    fix perm

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,6 +118,7 @@ DJOSER = {
     "LOGIN_FIELD": "email",
     "PERMISSIONS": {
         "user_list": ["rest_framework.permissions.AllowAny"],
+        "user": ["rest_framework.permissions.AllowAny"],
     },
 }
 
